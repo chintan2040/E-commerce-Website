90ff7b3edb55d31297e2254ee57e256215039b0b
chintan2040 <chintansopariwala2@gmail.com>
2025-07-16 22:28:10 +0530

    fourth commit

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,7 +1,9 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (user)=>{
-    return jwt.sign({email:user.email,id:user._id},process.env.JWT_SECRET);
+    return jwt.sign({email:user.email,id:user._id},process.env.JWT_SECRET,{
+        expiresIn: '1h'
+    });
 }
 
 module.exports.generateToken = generateToken;
